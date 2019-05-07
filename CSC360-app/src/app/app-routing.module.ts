8e2592180327d2eb05eeebbe6a15df02b0e1f545
david_kolschowsky <dlkolschowsky@gmail.com>
2019-05-06 23:49:49 -0500

    Finished Simple Migration of New trip page

--- CSC360-app/src/app/app-routing.module.ts
+++ CSC360-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {MyTripsComponent} from './mytrips/mytrips.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import {TriphomeComponent} from './triphome/triphome.component';
+import {NewtripComponent} from './newtrip/newtrip.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
   {path: 'mytrips', component: MyTripsComponent},
-  {path: 'triphome', component: TriphomeComponent}
+  {path: 'triphome', component: TriphomeComponent},
+  {path: 'newtrip', component: NewtripComponent}
 ];
 
 @NgModule({
--- CSC360-app/src/app/newtrip/newtrip.component.ts
+++ CSC360-app/src/app/newtrip/newtrip.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newtrip.component.css']
 })
 export class NewtripComponent implements OnInit {
-
+  tripDate = 'May-32-2019';
   constructor() { }
 
   ngOnInit() {
