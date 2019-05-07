4763b8081d6100dcf38a742275c945c0bf3acc90
david_kolschowsky <dlkolschowsky@gmail.com>
2019-05-07 15:53:28 -0500

    Fixed Flight results linking

--- CSC360-app/src/app/app-routing.module.ts
+++ CSC360-app/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {MyTripsComponent} from './mytrips/mytrips.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import {TriphomeComponent} from './triphome/triphome.component';
 import {NewtripComponent} from './newtrip/newtrip.component';
+import {FlightResultsComponent} from './flight-results/flight-results.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
   {path: 'mytrips', component: MyTripsComponent},
   {path: 'triphome', component: TriphomeComponent},
-  {path: 'newtrip', component: NewtripComponent}
+  {path: 'newtrip', component: NewtripComponent},
+  {path: 'flight-results', component: FlightResultsComponent}
 ];
 
 @NgModule({
