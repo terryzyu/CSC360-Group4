9123a395401fbcf19aca71b056d259d410a8541c
William Schroeder <wschroeder1519@gmail.com>
2019-05-07 20:06:26 -0500

    Added flight-search component Added flight-search to homepage added skeleton structure to login component

--- CSC360-app/src/app/app.module.ts
+++ CSC360-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TriphomeComponent } from './triphome/triphome.component';
 import { NewtripComponent } from './newtrip/newtrip.component';
 import { FlightResultsComponent } from './flight-results/flight-results.component';
 import { LoginComponent } from './login/login.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LoginComponent } from './login/login.component';
     TriphomeComponent,
     NewtripComponent,
     FlightResultsComponent,
-    LoginComponent
+    LoginComponent,
+    FlightSearchComponent
   ],
   imports: [
     BrowserModule,
