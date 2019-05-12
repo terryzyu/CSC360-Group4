3a3d5eeb432884ccff3e8233481fe74809a615ff
David Kolschowsky <dlkolschowsky@gmail.com>
2019-05-12 14:50:20 -0500

    Added trip.ts

---
+++ CSC360-app/src/app/trip.ts
@@ -0,0 +1,8 @@
+export class Trip {
+  constructor(
+    public startDate: Date =  new Date(),
+    public endDate: Date = new Date(),
+    public location: string = 'New York',
+    public budget: number = 0,
+  ) {}
+}
