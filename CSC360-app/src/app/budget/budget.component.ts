1a3220a1bd0e17b4ee6bf6f01d7055107cc6e8a7
david_kolschowsky <dlkolschowsky@gmail.com>
2019-05-12 22:12:17 -0500

    Fixed back functionality in budget component

--- CSC360-app/src/app/budget/budget.component.ts
+++ CSC360-app/src/app/budget/budget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 import {EVENTS} from '../mock-events';
 import {EventsService} from '../events.service';
@@ -12,7 +13,8 @@ import {nextContext} from '@angular/core/src/render3';
 export class BudgetComponent implements OnInit {
   Events = EVENTS;
 
-  constructor(private eventsService: EventsService) { }
+  constructor(private eventsService: EventsService,
+              private location: Location) { }
 
   ngOnInit() {
     this.getEvents();
@@ -20,5 +22,9 @@ export class BudgetComponent implements OnInit {
 
   private getEvents() {
     this.eventsService.getEvents().subscribe(event => this.Events = event);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
