b28f0adce50f8ef872b12c778085cf4c46e44325
Terry Yu <terryzyu@gmail.com>
2019-05-14 10:41:42 -0500

    Converted calendar into Angular (I think). Working on adding a button to add event

--- CSC360-app/src/app/calendar/calendar.component.ts
+++ CSC360-app/src/app/calendar/calendar.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Location} from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
+
 
 @Component({
   selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
+  templateUrl: './calendar.testing.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
