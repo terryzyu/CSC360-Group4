d2df6cf103d8e22552eb379f7784aae152321d92
david_kolschowsky <dlkolschowsky@gmail.com>
2019-06-06 20:38:14 -0500

    Update newtrips to utilize the geonames service. This way we know all the trips are going to valid locations. Thanks Will!

--- CSC360-app/src/app/newtrip/newtrip.component.ts
+++ CSC360-app/src/app/newtrip/newtrip.component.ts
@@ -4,6 +4,7 @@ import {FirebaseUTEService} from '../firebase-ute.service';
 import { Trip } from '../trip';
 import { Location } from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
+import {City, CityService} from '../city.service';
 
 
 @Component({
@@ -12,14 +13,17 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./newtrip.component.css']
 })
 export class NewtripComponent implements OnInit {
-  tripDate = 'May-32-2019';
   // @ts-ignore
   newTrip: Trip = new Trip( );
-  userName: string;
+  error: any;
+  cities: string[];
+  search: City;
+
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private fbUTEService: FirebaseUTEService) { }
+    private fbUTEService: FirebaseUTEService,
+    private cityService: CityService) { }
 
   ngOnInit() {
     this.newTrip.startDate.setFullYear(1970, 0, 1);
@@ -33,6 +37,25 @@ export class NewtripComponent implements OnInit {
 
   goBack() {
     this.location.back();
+  }
+
+  refreshCities(event) {
+    // TODO: update data on timeout
+
+    this.cityService.refreshCities(event.target.value).subscribe(( data: City ) => {
+        this.search = {...data};
+        console.log(this.search);
+
+        let i: number;
+        const size: number = Math.min(10, this.search.totalResultsCount);
+
+        this.cities = new Array(size);
+
+        for (i = 0; i < size; i++) {
+          this.cities[i] = this.search.geonames[i].name + ', ' + this.search.geonames[i].adminName1;
+        }
+      },
+      error => this.error = error);
   }
 
 }
